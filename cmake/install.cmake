a04e19b4acbcd68a4f0d6ad93116c064ed4654b7
KYANJO <brian.kyanjo@aims.ac.rw>
2022-10-16 22:31:04 -0600

    ctests implemented and working

---
+++ cmake/install.cmake
@@ -0,0 +1,71 @@
+# --- BOILERPLATE: install / packaging
+
+include(CMakePackageConfigHelpers)
+
+configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/config.cmake.in
+${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}Config.cmake
+INSTALL_DESTINATION cmake
+)
+
+write_basic_package_version_file(
+${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}ConfigVersion.cmake
+COMPATIBILITY SameMajorVersion
+)
+
+install(EXPORT ${PROJECT_NAME}-targets
+NAMESPACE ${PROJECT_NAME}::
+DESTINATION cmake
+)
+
+install(FILES
+${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}Config.cmake
+${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}ConfigVersion.cmake
+DESTINATION cmake
+)
+
+# --- CPack
+
+set(CPACK_GENERATOR "TGZ")
+set(CPACK_SOURCE_GENERATOR "TGZ")
+set(CPACK_PACKAGE_VENDOR "Carsten Burstedde;Donna Calhoun")
+set(CPACK_PACKAGE_CONTACT "Carsten Burstedde;Donna Calhoun")
+set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/COPYING")
+set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
+set(CPACK_PACKAGE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/package)
+string(TOLOWER ${CMAKE_SYSTEM_NAME} _sys)
+string(TOLOWER ${PROJECT_NAME} _project_lower)
+set(CPACK_PACKAGE_FILE_NAME "${_project_lower}-${git_version}-${_sys}")
+set(CPACK_SOURCE_PACKAGE_FILE_NAME "${_project_lower}-${git_version}")
+
+# not .gitignore as its regex syntax is more advanced than CMake
+set(CPACK_SOURCE_IGNORE_FILES .git/ .github/ .vscode/ _CPack_Packages/
+${CMAKE_BINARY_DIR}/ ${PROJECT_BINARY_DIR}/
+Makefile.in
+aclocal.m4
+autom4te.cache/
+build/
+build-aux/ar-lib
+build-aux/compile
+build-aux/config.guess
+build-aux/config.sub
+build-aux/depcomp
+build-aux/install-sh
+build-aux/ltmain.sh
+build-aux/missing
+build-aux/test-driver
+config/libtool.m4
+config/ltoptions.m4
+config/ltsugar.m4
+config/ltversion.m4
+config/lt~obsolete.m4
+configure
+src/sc_config_autotools.h.in
+scindent
+scspell
+)
+
+install(FILES ${CPACK_RESOURCE_FILE_README} ${CPACK_RESOURCE_FILE_LICENSE}
+DESTINATION share/docs/${PROJECT_NAME}
+)
+
+include(CPack)
---
+++ test/test.cpp
@@ -0,0 +1,122 @@
+#include <atomic>
+#include <fclaw_mpi.h>
+
+#define DOCTEST_CONFIG_IMPLEMENT
+#include <test.hpp>
+#include <exception>
+#include <csetjmp>
+
+static bool expect_abort=false;
+
+std::jmp_buf jump_buffer;
+
+void throw_exception()
+{
+    if(expect_abort)
+    {
+        expect_abort=false;
+        std::longjmp(jump_buffer, 1);
+    }
+}
+
+void fclaw_test_expect_abort()
+{
+    expect_abort=true;
+}
+
+void fclaw_test_clear_expect_abort()
+{
+    expect_abort=false;
+}
+
+std::jmp_buf& fclaw_test_get_jump_buffer()
+{
+    return jump_buffer;
+}
+
+int main(int argc, char *argv[])
+{
+    bool listing = false;
+    for (int i = 0; i < argc; i++) {
+        listing = strcmp(argv[i], "--list-test-cases") == 0;
+        if (listing)
+            break;
+        listing = strcmp(argv[i], "--list-test-suites") == 0;
+        if (listing)
+            break;
+        listing = strcmp(argv[i], "--list-reporters") == 0;
+        if (listing)
+            break;
+        listing = strcmp(argv[i], "-ltc") == 0;
+        if (listing)
+            break;
+        listing = strcmp(argv[i], "-lts") == 0;
+        if (listing)
+            break;
+        listing = strcmp(argv[i], "-ltr") == 0;
+        if (listing)
+            break;
+    }
+
+    doctest::Context context;
+    context.applyCommandLine(argc, argv);
+
+    int result = 0;
+
+    if(listing) {
+        int rank = 0;
+        if(rank == 0){
+	       result = context.run();
+        }
+    } else {
+	    // global setup...
+	    fclaw_mpi_init(nullptr, nullptr, sc_MPI_COMM_WORLD, SC_LP_PRODUCTION);
+
+        sc_set_abort_handler(throw_exception);
+	    result = context.run();
+
+	    // global clean-up...
+	    //fclaw_mpi_finalize();
+
+	    return result;
+    } 
+
+    // abort if failure, some tests can hang otherwise
+    if (!listing && (result > 0 || context.shouldExit())) {
+#ifdef P4EST_ENABLE_MPI
+        MPI_Abort(MPI_COMM_WORLD, result);
+#else
+        return -1;
+#endif
+    }
+
+    return 0;
+}
+
+#include <fclaw2d_domain.h>
+#include <fclaw2d_options.h>
+#include <fclaw2d_convenience.h>
+
+fclaw2d_domain_t* create_test_domain(sc_MPI_Comm mpicomm, fclaw_options_t* fclaw_opt)
+{
+    /* Mapped, multi-block domain */
+    p4est_connectivity_t     *conn = NULL;
+    fclaw2d_domain_t         *domain;
+    fclaw2d_map_context_t    *cont = NULL, *brick = NULL;
+    
+    int mi = fclaw_opt->mi;
+    int mj = fclaw_opt->mj;    
+    int a = 0; /* non-periodic */
+    int b = 0;
+
+    /* Square brick domain */
+    conn = p4est_connectivity_new_brick(mi,mj,a,b);
+    brick = fclaw2d_map_new_brick(conn,mi,mj);
+    cont = fclaw2d_map_new_nomap_brick(brick);
+    
+    domain = fclaw2d_domain_new_conn_map (mpicomm, fclaw_opt->minlevel, conn, cont);
+    fclaw2d_domain_list_levels(domain, FCLAW_VERBOSITY_ESSENTIAL);
+    fclaw2d_domain_list_neighbors(domain, FCLAW_VERBOSITY_DEBUG);
+    
+    return domain;    
+}
