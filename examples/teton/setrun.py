3815f27015b6c3731f28c94f6ea079e8871bac7a
KYANJO <brian.kyanjo@aims.ac.rw>
2022-11-05 21:31:35 -0600

    able to simulate the teton dambreak after adjusting the manning coefficient

--- examples/teton/setrun.py
+++ examples/teton/setrun.py
@@ -176,7 +176,7 @@ def setrun(claw_pkg='geoclaw'):
 
     if clawdata.output_style == 1:
         # Output nout frames at equally spaced times up to tfinal:
-        n_hours = 2.0
+        n_hours = 7.0
         frames_per_minute = 60.0/30.0 # Frames every 5 seconds
         clawdata.num_output_times = int(frames_per_minute*60*n_hours)  # Plot every 10 seconds
         clawdata.tfinal = 60*60*n_hours
@@ -431,35 +431,35 @@ def setrun(claw_pkg='geoclaw'):
     # Start at SW corner; build gauges in counter-clockwise order in a
     # square around the region [xll,xur].
     #-------------------------------------------------------
-    # m = 2  # Gauge spacing along one edge (m=4 --> edge divided into four sections)
-    # gauge_counter = 100
+    m = 2  # Gauge spacing along one edge (m=4 --> edge divided into four sections)
+    gauge_counter = 100
 
-    # # South West corner of power plant
-    # xll = [-111.623926, 43.913661]  # From email
+    # South West corner of power plant
+    xll = [-111.623926, 43.913661]  # From email
 
-    # # North East corner of power plant
-    # xur = [-111.620150, 43.916382]  # from email
+    # North East corner of power plant
+    xur = [-111.620150, 43.916382]  # from email
 
-    # s = np.linspace(0,1.,m+1)
-    # for i in range(0,m):
-    #     x = xll[0] + (xur[0] - xll[0])*s[i]
-    #     rundata.gaugedata.gauges.append([gauge_counter,x,xll[1],0.,clawdata.tfinal])
-    #     gauge_counter = gauge_counter + 1
+    s = np.linspace(0,1.,m+1)
+    for i in range(0,m):
+        x = xll[0] + (xur[0] - xll[0])*s[i]
+        rundata.gaugedata.gauges.append([gauge_counter,x,xll[1],0.,clawdata.tfinal])
+        gauge_counter = gauge_counter + 1
 
-    # for i in range(0,m):
-    #     y = xll[1] + (xur[1] - xll[1])*s[i]
-    #     rundata.gaugedata.gauges.append([gauge_counter,xur[0],y,0.,clawdata.tfinal])
-    #     gauge_counter = gauge_counter + 1
+    for i in range(0,m):
+        y = xll[1] + (xur[1] - xll[1])*s[i]
+        rundata.gaugedata.gauges.append([gauge_counter,xur[0],y,0.,clawdata.tfinal])
+        gauge_counter = gauge_counter + 1
 
-    # for i in range(0,m):
-    #     x = xur[0] + (xll[0] - xur[0])*s[i]
-    #     rundata.gaugedata.gauges.append([gauge_counter,x,xur[1],0.,clawdata.tfinal])
-    #     gauge_counter = gauge_counter + 1
+    for i in range(0,m):
+        x = xur[0] + (xll[0] - xur[0])*s[i]
+        rundata.gaugedata.gauges.append([gauge_counter,x,xur[1],0.,clawdata.tfinal])
+        gauge_counter = gauge_counter + 1
 
-    # for i in range(0,m):
-    #     y = xur[1] + (xll[1] - xur[1])*s[i]
-        # rundata.gaugedata.gauges.append([gauge_counter,xll[0],y,0.,clawdata.tfinal])
-        # gauge_counter = gauge_counter + 1
+    for i in range(0,m):
+        y = xur[1] + (xll[1] - xur[1])*s[i]
+        rundata.gaugedata.gauges.append([gauge_counter,xll[0],y,0.,clawdata.tfinal])
+        gauge_counter = gauge_counter + 1
 
 
     # -------------------------------------------------------
