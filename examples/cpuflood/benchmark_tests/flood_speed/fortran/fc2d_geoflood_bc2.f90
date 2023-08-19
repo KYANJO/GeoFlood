ac697fdf3e32a78578ef30977a79a6bf20080718
KYANJO <brian.kyanjo@aims.ac.rw>
2023-08-18 18:48:19 -0600

    updated bc conditions for benchmarks

--- examples/cpuflood/benchmark_tests/flood_speed/fortran/fc2d_geoflood_bc2.f90
+++ examples/cpuflood/benchmark_tests/flood_speed/fortran/fc2d_geoflood_bc2.f90
@@ -56,51 +56,58 @@ subroutine flood_speed_bc2(meqn,mbc,mx,my,xlower,ylower,dx,dy,q,maux,aux,t,dt,mt
         
         if (abs(y-1000.0d0) <= 10.0d0) then
         
-            q(1,1,j) = max(q(1,1,j), 0.001d0)
+            ! q(1,1,j) = max(q(1,1,j), 0.001d0)
 
-            if (hu_0 .ge. 0.0d0) then 
+            ! if (hu_0 .ge. 0.0d0) then 
                 
-                do ibc=1,mbc
-
-                    u_1 = q(2,1,j)/q(1,1,j)
-                    
-                    ! if (hu_0 .ne. 0.0d0) then
-                        ! call newton_raphson(h_0,hu_0,q(1,1,j),u_1)
-                        ! if (h_0 > q(1,1,j)) then
-                        !     call two_shock(h_0,hu_0,q(1,1,j),u_1)
-                        ! end if
-                    h1 = q(1,1,j)
-                    u_0 = hu_0/h1
-                    do i = 1,100
-                        h_0 = ((u_0 - u_1 + 2*sqrt(g*h1))**2)/(4.0d0*g)
-                        if (h_0 .le. 0) then
-                            h_0 = 0
-                            u_0 = 0
-                        else
-                            if (abs((hu_0/h_0) - u_0) < 1.0d-6) exit
-                                u_0 = hu_0/h_0
-                        end if
-                    enddo
-
+            do ibc=1,mbc
+                if (q(1,1,j) < dry_tolerance) then
+                    h_0 = max((hu_0/sqrt(g))**(2.0d0/3.0d0), 0.001d0) 
                     q(1,1-ibc,j) = h_0
                     q(2,1-ibc,j) = hu_0
                     q(3,1-ibc,j) = 0.0d0
-            
-                enddo
-            end if
+                else 
+                    u_1 = q(2,1,j)/q(1,1,j)
+                    if (hu_0 .ne. 0.0d0) then
+                        call newton_raphson(h_0,hu_0,q(1,1,j),u_1)
+                        if (h_0 > q(1,1,j)) then
+                            call two_shock(h_0,hu_0,q(1,1,j),u_1) ! entropy fix
+                        end if
+                        ! h1 = q(1,1,j)
+                        ! u_0 = hu_0/h1
+                        ! do i = 1,100
+                        !     h_0 = ((u_0 - u_1 + 2*sqrt(g*h1))**2)/(4.0d0*g)
+                        !     if (h_0 .le. 0) then
+                        !         h_0 = 0
+                        !         u_0 = 0
+                        !     else
+                        !         if (abs((hu_0/h_0) - u_0) < 1.0d-6) exit
+                        !             u_0 = hu_0/h_0
+                        !     end if
+                        ! enddo
+
+                        q(1,1-ibc,j) = h_0
+                        q(2,1-ibc,j) = hu_0
+                        q(3,1-ibc,j) = 0.0d0
+                    else
+                        aux(1,1-ibc,j) = aux(1,1,j)
+                        do m=1,meqn
+                            q(m,1-ibc,j) = q(m,1,j)
+                        enddo
+                    endif
+                endif 
+            enddo
 
         ! ---------- end working bc -------------
         else
             do ibc=1,mbc
-                    
+                aux(1,1-ibc,j) = aux(1,ibc,j)
+                do m=1,meqn
+                    q(m,1-ibc,j) = q(m,ibc,j)
+                enddo
 
-                        aux(1,1-ibc,j) = aux(1,ibc,j)
-                        do m=1,meqn
-                            q(m,1-ibc,j) = q(m,ibc,j)
-                        enddo
-
-                        ! c     # negate the normal velocity:   
-                        q(2,1-ibc,j) = -q(2,ibc,j)
+                ! c     # negate the normal velocity:   
+                q(2,1-ibc,j) = -q(2,ibc,j)
                 ! end if
             enddo
         endif
