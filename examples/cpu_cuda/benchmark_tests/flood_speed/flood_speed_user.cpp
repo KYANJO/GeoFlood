233bc845ed94498dc59d779c4dea73caf517c776
KYANJO <brian.kyanjo@aims.ac.rw>
2024-01-08 11:44:48 -0700

    using the global BCs with hydrograph supported

--- examples/cpu_cuda/benchmark_tests/flood_speed/flood_speed_user.cpp
+++ examples/cpu_cuda/benchmark_tests/flood_speed/flood_speed_user.cpp
@@ -49,19 +49,19 @@ void flood_speed_link_solvers(fclaw2d_global_t *glob)
 
     /* These are set by GeoClaw for convenience, but the user
        can set these with customized functions, if desired. */
-    user_options_t* user_opt = flood_speed_get_options(glob);
-    if (user_opt->cuda == 0)
-    {
-        fc2d_geoclaw_vtable_t* geoclaw_vt = fc2d_geoclaw_vt(glob);
-        // geoclaw_vt->qinit = &FLOOD_SPEED_QINIT; /* initial conditions */
-        geoclaw_vt->bc2 = &FLOOD_SPEED_BC2; /* special BC at the left boundary */
-    }
-    else 
-    {
-        fc2d_geoclaw_vtable_t *cuclaw_vt = fc2d_geoclaw_vt(glob);
-        // cuclaw_vt->fort_bc2 = &CUDACLAW_BC2;
-        cuclaw_vt->bc2 = &FLOOD_SPEED_BC2;
-        // cuclaw_vt->qinit = &FLOOD_SPEED_QINIT;
-    }
+    // user_options_t* user_opt = flood_speed_get_options(glob);
+    // if (user_opt->cuda == 0)
+    // {
+    //     fc2d_geoclaw_vtable_t* geoclaw_vt = fc2d_geoclaw_vt(glob);
+    //     // geoclaw_vt->qinit = &FLOOD_SPEED_QINIT; /* initial conditions */
+    //     // geoclaw_vt->bc2 = &FLOOD_SPEED_BC2; /* special BC at the left boundary */
+    // }
+    // else 
+    // {
+    //     fc2d_geoclaw_vtable_t *cuclaw_vt = fc2d_geoclaw_vt(glob);
+    //     // cuclaw_vt->fort_bc2 = &CUDACLAW_BC2;
+    //     // cuclaw_vt->bc2 = &FLOOD_SPEED_BC2;
+    //     // cuclaw_vt->qinit = &FLOOD_SPEED_QINIT;
+    // }
     
 }
