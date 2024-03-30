a126fe4f343ce679dfc3729fcc3c0ec8893bdb00
kyanjo <brian.kyanjo@aims.ac.rw>
2024-03-30 11:37:51 -0600

    wip (cuda) plotting tools

--- examples/cpu_cuda/missoula/make_plots_kml.py
+++ examples/cpu_cuda/missoula/make_plots_kml.py
@@ -283,7 +283,7 @@ def setplot(plotdata):
 
     plotdata.parallel = False
     plotdata.print_format = 'png'           # file format
-    plotdata.print_framenos = range(0,100,5)         # list of frames to print
+    plotdata.print_framenos = range(0,100,1)         # list of frames to print
     plotdata.print_gaugenos = 'all'         # list of gauges to print
     plotdata.print_fignos = [1,300]         # list of figures to print
 
